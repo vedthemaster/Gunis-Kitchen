638d23bd04624dd62067f9699c5ebae29674da42
Ved Patel <vedpatel3395@gmail.com>
2022-03-02 20:27:36 +0530

    Displayed Items on Index

---
+++ Gunis.Kitchen/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Gunis.Kitchen.Models;
+using Gunis.Kitchen.Data;
+using Microsoft.AspNetCore.Hosting;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gunis.Kitchen.Controllers
+{
+    public class UserController : Controller
+    {
+
+        private readonly ApplicationDbContext _context;
+        private readonly IWebHostEnvironment _hostEnvironment;
+
+        public UserController(ApplicationDbContext context, IWebHostEnvironment hostEnvironment)
+        {
+            _context = context;
+            this._hostEnvironment = hostEnvironment;
+        }
+
+        // GET: Items
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Items.Include(i => i.Category);
+            return View(await applicationDbContext.ToListAsync());
+        }
+    }
+}
