30f935364ff5202269ff80c82bd773190b43de2b
Ved Patel <vedpatel3395@gmail.com>
2022-03-08 08:40:31 +0530

    Made some changes

--- Gunis.Kitchen/Controllers/OrderController.cs
+++ Gunis.Kitchen/Controllers/OrderController.cs
@@ -1,4 +1,4 @@
-﻿using Gunis.Kitchen.Interfaces;
+﻿
 using Gunis.Kitchen.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -8,12 +8,12 @@ namespace Gunis.Kitchen.Controllers
     [Authorize(Roles = "User")]
     public class OrderController : Controller
     {
-        private readonly IOrderRepository _orderRepository;
+
         private readonly ShoppingCart _shoppingCart;
 
-        public OrderController(IOrderRepository orderRepository,ShoppingCart shoppingCart)
+        public OrderController(ShoppingCart shoppingCart)
         {
-            _orderRepository = orderRepository;
+            
             _shoppingCart = shoppingCart;
         }
         public IActionResult Checkout()
