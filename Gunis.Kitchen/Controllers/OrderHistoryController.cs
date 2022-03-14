155c4dd1d890fb33857716074569126b7d24375b
Ved Patel <vedpatel3395@gmail.com>
2022-03-14 13:36:43 +0530

    Working On Order History

---
+++ Gunis.Kitchen/Controllers/OrderHistoryController.cs
@@ -0,0 +1,32 @@
+﻿using Gunis.Kitchen.Data;
+using Gunis.Kitchen.Models;
+using Gunis.Kitchen.ViewModel;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gunis.Kitchen.Controllers
+{
+    public class OrderHistoryController : Controller
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+        private readonly UserManager<MyIdentityUser> _userManager;
+
+        public OrderHistoryController(ApplicationDbContext applicationDbContext,UserManager<MyIdentityUser> userManager)
+        {
+            _applicationDbContext = applicationDbContext;
+            _userManager = userManager;
+        }
+        public async Task<IActionResult> Index()
+        {
+
+            var orderHVM = new OrderHistoryViewModel();
+            var user = await _userManager.GetUserAsync(this.User);
+
+            var order = _applicationDbContext.Orders.FirstOrDefault(o => o.OrderId == orderHVM.OrderId);
+
+            return View(orderHVM);
+        }
+    }
+}
---
+++ Gunis.Kitchen/ViewModel/OrderHistoryViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Gunis.Kitchen.Models;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Gunis.Kitchen.ViewModel
+{
+    public class OrderHistoryViewModel
+    {
+        public IEnumerable<Order> Orders { get; set; }
+
+        public  string OrderId { get; set; }
+
+        public IEnumerable<OrderDetail> OrderDetails { get; set; }
+
+    }
+}
